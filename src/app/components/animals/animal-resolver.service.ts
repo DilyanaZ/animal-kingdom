62cdddf9fbfbe83e4eac5d808bb4e9a6465d9613
Dilyana P. Zheleva <dilyana.zheleva@musala.com>
2019-03-18 17:20:50 +0200

    formatting, removed redundant blank line

--- src/app/components/animals/animal-resolver.service.ts
+++ src/app/components/animals/animal-resolver.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@angular/core";
+import { Injectable, forwardRef } from "@angular/core";
 import {
   Resolve,
   ActivatedRouteSnapshot,
@@ -6,8 +6,7 @@ import {
 } from "@angular/router";
 import { AnimalsService } from "./animals.service";
 import { Observable } from "rxjs";
-import { Animal } from "../models/animal";
-import { tap, map } from "rxjs/operators";
+import {  map } from "rxjs/operators";
 
 @Injectable({
   providedIn: "root"
@@ -16,10 +15,15 @@ export class AnimalDetailsResolver implements Resolve<any> {
   constructor(public animalsService: AnimalsService) {}
 
   resolve(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
+    route: ActivatedRouteSnapshot, //currently activated route
+    state: RouterStateSnapshot //reprisent the state of the application router, it is a tree of ActivatedRouteSnapshots
   ): Observable<any> {
     const id = route.paramMap.get("id");
-    return this.animalsService.getAnimalDetails(id);
+    //return this.animalsService.getAnimalDetails(route.params.id);
+    return this.animalsService
+      .getAnimalDetails(id)
+      .pipe(map(animal => ({ animal: animal })));
   }
 }
+
+
